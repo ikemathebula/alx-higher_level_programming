f86eba7a9527ed30260ace462918da8f060a968a
ikemathebula <ikemathebula@gmail.com>
2023-01-09 06:47:25 -0800

    0x03-python-data_structures

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,54 +1,114 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include "lists.h"
 
+/**
+ * reverse - reverses the second half of the list
+ *
+ * @h_r: head of the second half
+ * Return: no return
+ */
+void reverse(listint_t **h_r)
+{
+	listint_t *prv;
+	listint_t *crr;
+	listint_t *nxt;
+
+	prv = NULL;
+	crr = *h_r;
+
+	while (crr != NULL)
+	{
+		nxt = crr->next;
+		crr->next = prv;
+		prv = crr;
+		crr = nxt;
+	}
+
+	*h_r = prv;
+}
+
+/**
+ * compare - compares each int of the list
+ *
+ * @h1: head of the first half
+ * @h2: head of the second half
+ * Return: 1 if are equals, 0 if not
+ */
+int compare(listint_t *h1, listint_t *h2)
+{
+	listint_t *tmp1;
+	listint_t *tmp2;
+
+	tmp1 = h1;
+	tmp2 = h2;
+
+	while (tmp1 != NULL && tmp2 != NULL)
+	{
+		if (tmp1->n == tmp2->n)
+		{
+			tmp1 = tmp1->next;
+			tmp2 = tmp2->next;
+		}
+		else
+		{
+			return (0);
+		}
+	}
+
+	if (tmp1 == NULL && tmp2 == NULL)
+	{
+		return (1);
+	}
+
+	return (0);
+}
+
+/**
+ * is_palindrome - checks if a singly linked list
+ * is a palindrome
+ * @head: pointer to head of list
+ * Return: 0 if it is not a palindrome,
+ * 1 if it is a palndrome
+ */
 int is_palindrome(listint_t **head)
 {
-	// Check for empty list or single-node list
-	if (*head == NULL || (*head)->next == NULL)
-		return 1;
+	listint_t *slow, *fast, *prev_slow;
+	listint_t *scn_half, *middle;
+	int isp;
 
-	// Find the middle of the list
-	listint_t *slow = *head, *fast = *head;
-	while (fast && fast->next)
+	slow = fast = prev_slow = *head;
+	middle = NULL;
+	isp = 1;
+
+	if (*head != NULL && (*head)->next != NULL)
 	{
-		slow = slow->next;
-		fast = fast->next->next;
+		while (fast != NULL && fast->next != NULL)
+		{
+			fast = fast->next->next;
+			prev_slow = slow;
+			slow = slow->next;
+		}
+
+		if (fast != NULL)
+		{
+			middle = slow;
+			slow = slow->next;
+		}
+
+		scn_half = slow;
+		prev_slow->next = NULL;
+		reverse(&scn_half);
+		isp = compare(*head, scn_half);
+
+		if (middle != NULL)
+		{
+			prev_slow->next = middle;
+			middle->next = scn_half;
+		}
+		else
+		{
+			prev_slow->next = scn_half;
+		}
 	}
 
-	// Reverse the second half of the list
-	listint_t *prev = NULL, *curr = slow, *next = slow->next;
-	while (next)
-	{
-		curr->next = prev;
-		prev = curr;
-		curr = next;
-		next = next->next;
-	}
-	curr->next = prev;
-
-	// Compare the first and second halves of the list
-	listint_t *left = *head, *right = curr;
-	while (right)
-	{
-		if (left->n != right->n)
-			return 0;
-		left = left->next;
-		right = right->next;
-	}
-
-	// Restore the original list
-	prev = NULL;
-	curr = slow;
-	next = slow->next;
-	while (next)
-	{
-		curr->next = prev;
-		prev = curr;
-		curr = next;
-		next = next->next;
-	}
-	curr->next = prev;
-
-	return 1;
+	return (isp);
 }
