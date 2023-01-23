e84ee1b53c8ddbd22ea1fb459e791aa9bda67590
ikemathebula <ikemathebula@gmail.com>
2023-01-23 17:49:19 +0000

    python-exceptions

--- 0x05-python-exceptions/103-python.c
+++ 0x05-python-exceptions/103-python.c
@@ -1,4 +1,4 @@
-1#include <stdio.h>
+#include <stdio.h>
 #include <Python.h>
 
 /**
--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -1,4 +1,4 @@
-1#!/usr/bin/python3
+#!/usr/bin/python3
 def safe_print_division(a, b):
     """Returns the division of a by b."""
     try:
