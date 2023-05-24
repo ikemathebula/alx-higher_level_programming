e9f68ed0bf4f83c58ecf36b626f1de69ef9118eb
ikemathebula <ikemathebula@gmail.com>
2023-05-24 14:41:10 +0000

    javascript-web_scraping

---
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -0,0 +1,8 @@
+t request = require('request');
+const fs = require('fs');
+
+request(process.argv[2], function (err, response, body) {
+  if (err == null) {
+    fs.writeFileSync(process.argv[3], body);
+  }
+});
